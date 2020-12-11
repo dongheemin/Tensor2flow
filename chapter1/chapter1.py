f545ee2f6c30c1c9175c680eb87d9774683ca8dc
dongheemin <crydevil5@nate.com>
2020-12-11 19:51:13 +0900

    chapter1

--- chapter1/chapter1.py
+++ chapter1/chapter1.py
@@ -48,10 +48,10 @@ test_labels
 
 # Visualization Dataset[0-9]
 for i in range (0, 10):
-	plt.figure(figsize=(1,1))
+	plt.figure(figsize=(0.3,0.3))
 	plt.imshow(train_images[i])
 	plt.colorbar()
 	plt.grid(False)
-	plt.savefig('./images/fig'+str(i)+'.png', dpi=300)
+	plt.savefig('./images/fig'+str(i)+'.png')
 
 
